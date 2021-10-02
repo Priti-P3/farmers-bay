fa4a7d2428e3df7303dd191eef951fdb08c85d83
Aashish Lodha <lodhaaashish@gmail.com>
2021-10-02 17:07:07 +0530

    categories icons added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,12 +9,15 @@ export class Tab1Page {
   categories: any[] = [
     {
       "name" : "Fertilizers",
+      "icon" : "../../assets/icon/fertilizers-1.png"
     },
     {
       "name" : "Pesticides",
+      "icon" : "../../assets/icon/pesticides-1.png"
     },
     {
       "name" : "Seeds",
+      "icon" : "../../assets/icon/seeds-1.png"
     },
     
   ];
