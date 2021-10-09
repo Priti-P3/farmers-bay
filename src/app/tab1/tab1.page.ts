3c214c14054dfc824ce8411dbe01d73468210230
Aashish Lodha <lodhaaashish@gmail.com>
2021-10-10 01:51:49 +0530

    Http call added for categories

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,21 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  categories: any[] = [
-    {
-      "name" : "Fertilizers",
-      "icon" : "../../assets/icon/fertilizers-1.png"
-    },
-    // {
-    //   "name" : "Pesticides",
-    //   "icon" : "../../assets/icon/pesticides-1.png"
-    // },
-    // {
-    //   "name" : "Seeds",
-    //   "icon" : "../../assets/icon/seeds-1.png"
-    // },
+  // categories: any[] = [
+  //   {
+  //     "categoryName" : "Fertilizers",
+  //     "iconPath" : "../../assets/icon/fertilizers-1.png",
+  //     "categoryId": 1
+  //   },
+  //   {
+  //     "categoryName" : "Pesticides",
+  //     "iconPath" : "../../assets/icon/pesticides-1.png",
+  //     "categoryId": 2
+  //   },
+  //   {
+  //     "categoryName" : "Seeds",
+  //     "iconPath" : "../../assets/icon/seeds-1.png",
+  //     "categoryId": 3
+  //   },
     
-  ];
-  constructor() {}
+  // ];
+
+  categories: any;
+  constructor(private http: HttpClient) {
+    http.get("http://localhost:8080/category").subscribe((data) => {
+      this.categories = data;
+    });
+  }
 
 }
